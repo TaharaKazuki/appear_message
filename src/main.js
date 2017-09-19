7dac7284eae3cbf8cd82ac9b025b124f5bf90518
田原　一樹 <k.tahara@leihauoli.com>
2017-09-19 09:40:04 +0900

    機能実装

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import css from './scss/main.scss';
+import jQuery from 'jquery';
+const $ = jQuery;
+
+class captionMotion {
+	constructor(){
+		this.init();
+	}
+	init(){
+		this.setParameter();
+		this.bindEvent();
+	}
+	setParameter(){
+		this.$caption = $('.myText');
+		this.text = this.$caption.text();
+		this.$caption.empty().show();
+		this.array = this.text.split('');
+		this.element = [];
+		this.$startBtn = $('.jsc-Btn');
+	}
+	bindEvent(){
+		this.array.map((item,index)=>{
+			this.element[index] = $(`<span>${item}</span>`);
+			this.$caption.append(this.element[index]);
+		});
+		this.$startBtn.on('click',$.proxy(this.motion,this));
+	}
+	motion(){
+		this.$startBtn.addClass('isHide');
+		this.element.map((item,index)=>{
+			item.delay(200 * index).queue(function(){
+				$(this).addClass('motion');
+			});
+		});
+	}
+};
+
+new captionMotion;
