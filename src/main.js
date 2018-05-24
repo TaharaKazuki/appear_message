977de589c41bddd808065f11e7829d38b599ab6e
田原 一樹 <ssp0727.lnc@gmail.com>
2018-05-25 02:13:38 +0900

    fix file インデント修正

--- src/main.js
+++ src/main.js
@@ -1,38 +1,37 @@
-import css from './scss/main.scss';
 import jQuery from 'jquery';
 const $ = jQuery;
 
 class captionMotion {
   constructor(){
-      this.init();
+    this.init();
   }
   init(){
-      this.setParameter();
-      this.bindEvent();
+    this.setParameter();
+    this.bindEvent();
   }
   setParameter(){
-      this.$caption = $('.myText');
-      this.text = this.$caption.text();
-      this.$caption.empty().show();
-      this.array = this.text.split('');
-      this.element = [];
-      this.$startBtn = $('.jsc-Btn');
+    this.$caption = $('.myText');
+    this.text = this.$caption.text();
+    this.$caption.empty().show();
+    this.array = this.text.split('');
+    this.element = [];
+    this.$startBtn = $('.jsc-Btn');
   }
   bindEvent(){
-      this.array.map((item,index)=>{
-          this.element[index] = $(`<span>${item}</span>`);
-          this.$caption.append(this.element[index]);
-      });
-      this.$startBtn.on('click',$.proxy(this.motion,this));
+    this.array.map((item,index)=>{
+      this.element[index] = $(`<span>${item}</span>`);
+      this.$caption.append(this.element[index]);
+    });
+    this.$startBtn.on('click',$.proxy(this.motion,this));
   }
   motion(){
-      this.$startBtn.addClass('isHide');
-      this.element.map((item,index)=>{
-          item.delay(200 * index).queue(function(){
-              $(this).addClass('motion');
-          });
+    this.$startBtn.addClass('isHide');
+    this.element.map((item,index)=>{
+      item.delay(200 * index).queue(function(){
+        $(this).addClass('motion');
       });
+    });
   }
-};
+}
 
 new captionMotion;
